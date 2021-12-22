e7dcccf3047554065cc8174293fb5d78f28bcc81
Ishan Gogna <ig2230@nyu.edu>
2021-12-22 16:16:50 -0500

    testing

--- my-app/src/views/resultsPage.js
+++ my-app/src/views/resultsPage.js
@@ -2,10 +2,26 @@ import React from 'react';
 import ResponsiveAppBar from '../components/navbar';
 
 const ResultsPage = () => {
+    const callMethod = () => {
+        const body = {
+            "username" : "thefriendlyuser",
+            "password" : "Password1"
+        }
+        const url = "https://e2rfuo7dnj.execute-api.us-east-1.amazonaws.com/prod/login"
+        fetch(url, {
+            method : "POST",
+            headers : {
+                'content-type' : 'application/json',
+            },
+            body : JSON.stringify(body)
+        }).then(response => response.json())
+        .then(data => console.log(data))
+        .catch(err => console.log('error : ' + err));
+    }
     return (
         <div>
             <ResponsiveAppBar />
-            Results Page
+            <div onClick = {()=> {callMethod()}}>Click me</div>
         </div>
     )
 }
